40daf2f680075dcb76b6d6bc1e9ff50ef52ea5d3
Cheng Bi <ChengBi@users.noreply.github.com>
2017-05-20 13:26:40 +0100

    Create model.py

---
+++ model.py
@@ -0,0 +1,5 @@
+class model(object):
+    
+    def __init__(self,placeholder_input,placeholder_target):
+        self.placeholder_input=placeholder_input
+        self.placeholder_target=placeholder_target
